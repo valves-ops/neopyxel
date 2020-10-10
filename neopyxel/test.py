f7a8aa2eafee100e294fa9a7b684c601468b18a9
vinicius Domingos Alves <vinicius.alves@navarratech.com>
2020-10-10 01:45:16 -0300

    updated test with new relay functions

--- neopyxel/test.py
+++ neopyxel/test.py
@@ -1,18 +1,26 @@
 import neopyxel
-import time 
+import time
 
 relay = neopyxel.NeopyxelRelay()
 relay.add_stripe(30, 4)
 relay.add_stripe(30, 5)
+relay.add_stripe(30, 6)
 
-for stripe in relay._stripes:
-    print(stripe.stripe_number)
-    stripe.setPixelColor(list(range(0,30)), (0,0,0))
-    print('OFF')
-    stripe.show()
-    time.sleep(2)
-    stripe.setPixelColor(list(range(0,30)), (120,120,120))
-    print('ON')
-    stripe.show()
+relay.set_pixel_color(list(range(0, 30)), (0, 0, 0))
+relay.show()
+time.sleep(1)
+relay.set_pixel_color(list(range(0, 30)), (240, 80, 30))
+relay.show()
 
+# for stripe in relay._stripes:
+#     print(stripe.stripe_number)
+#     stripe.set_pixel_color(list(range(0,30)), (0,0,0))
+#     print('OFF')
+#     stripe.show()
+#     time.sleep(0.5)
+#     stripe.set_pixel_color(list(range(0,30)), (240,80,30))
+#     print('ON')
+#     #stripe.show()
 
+# for stripe in relay._stripes:
+#     stripe.show()

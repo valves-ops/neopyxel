96735ff40a4382eea012b6d1cca71ea5652f8c78
vinicius Domingos Alves <vinicius.alves@navarratech.com>
2020-10-10 12:17:29 -0300

    Organized imports, private attributes and properties

--- neopyxel/neopyxel.py
+++ neopyxel/neopyxel.py
@@ -1,9 +1,8 @@
 import time
-import serial
-import serial.tools.list_ports
 import itertools
 from collections.abc import Iterable
-
+import serial
+import serial.tools.list_ports
 
 class NeopyxelRelay():
     def __init__(self, serial_port=None):
@@ -12,52 +11,72 @@ class NeopyxelRelay():
             for comport in comports:
                 if comport.pid is not None:
                     serial_port = comport.device
-        self.serial_port = serial_port
-        self.conn = serial.Serial(self.serial_port, 28800, writeTimeout=0)
+        self.__serial_port = serial_port
+        self.__conn = serial.Serial(self.__serial_port, 28800, writeTimeout=0)
         time.sleep(1.8)
-        self._stripes = []
+        self.__stripes = []
+
+    @property
+    def stripes(self):
+        return self.__stripes
 
     def add_stripe(self, NUMPIXELS, PIN):
-        self._stripes.append(Stripe(NUMPIXELS, PIN, self.conn))
+        self.__stripes.append(Stripe(NUMPIXELS, PIN, self.__conn))
         cmd = bytearray(4)
         cmd[0] = 0
         cmd[1] = 3
         cmd[2] = NUMPIXELS
         cmd[3] = PIN
-        self.conn.write(cmd)
+        self.__conn.write(cmd)
 
     def set_pixel_color(self, pixel_number, color):
         if isinstance(pixel_number, Iterable):
             for pixel in pixel_number:
                 self.set_pixel_color(pixel, color)
         else:
-            for stripe in self._stripes:
+            for stripe in self.__stripes:
                 stripe.set_pixel_color(pixel_number, color)
 
     def show(self):
-        for stripe in self._stripes:
+        for stripe in self.__stripes:
             stripe.show()
 
-    def clear_stripes(self):
+    def flush_stripes(self):
         cmd = bytearray(4)
         cmd[0] = 0
         cmd[1] = 4
-        self.conn.write(cmd)
+        self.__conn.write(cmd)
 
     def __del__(self):
-        self.clear_stripes()
-        self.conn.close()
+        self.flush_stripes()
+        self.__conn.close()
 
 
 class Stripe:
     counter = itertools.count()
 
     def __init__(self, NUMPIXELS, PIN, conn):
-        self.NUMPIXELS = NUMPIXELS
-        self.pixels = [VirtualPixel()]*self.NUMPIXELS
-        self.PIN = PIN
-        self.stripe_number = next(Stripe.counter)
-        self.conn = conn
+        self.__NUMPIXELS = NUMPIXELS
+        self.__pixels = [VirtualPixel()]*self.__NUMPIXELS
+        self.__PIN = PIN
+        self.__stripe_number = next(Stripe.counter)
+        self.__conn = conn
+
+    @property
+    def pixels(self):
+        return self.__pixels
+
+    @property
+    def stripe_number(self):
+        return self.__stripe_number
+    
+    @property
+    def num_pixels(self):
+        return self.__NUMPIXELS
+
+    @property
+    def pin(self):
+        return self.__PIN
 
     def set_pixel_color(self, pixel_number, color):
         if isinstance(pixel_number, Iterable):
@@ -72,31 +91,34 @@ class Stripe:
             cmd[3] = color[0]
             cmd[4] = color[1]
             cmd[5] = color[2]
-            self.conn.write(cmd)
-            self.pixels[pixel_number].set_pixel_color(color)
-            # time.sleep(0.01)
+            self.__conn.write(cmd)
+            self.__pixels[pixel_number].set_pixel_color(color)
+            time.sleep(0.01)
 
     def show(self):
         cmd = bytearray(2)
         cmd[0] = self.stripe_number
         cmd[1] = 2  # Show Command code
-        self.conn.write(cmd)
+        self.__conn.write(cmd)
         for pixel in self.pixels:
             pixel.show()
 
 
 class VirtualPixel:
     def __init__(self, color=(0, 0, 0)):
-        self.displayed_color = color
-        self.buffer_color = color
-        self.coherent = True
+        self.__displayed_color = color
+        self.__buffer_color = color
+        self.__coherent = True
 
     def set_pixel_color(self, color):
-        self.buffer_color = color
-        if self.buffer_color != self.displayed_color:
-            self.coherent = False
+        self.__buffer_color = color
+        if self.__buffer_color != self.__displayed_color:
+            self.__coherent = False
 
     def show(self):
-        if not self.coherent:
-            self.displayed_color = self.buffer_color
-            self.coherent = True
+        if not self.__coherent:
+            self.__displayed_color = self.__buffer_color
+            self.__coherent = True
+
+    def __str__(self):
+        return '({}, {}, {})'.format(*self.__displayed_color)

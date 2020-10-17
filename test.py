c647ed756dc5322c9a21a6ffacf7aaa42304d1fd
vinicius Domingos Alves <vinicius.alves@navarratech.com>
2020-10-17 11:07:46 -0300

    Added keyboardInterrupt thread termination code

--- test.py
+++ test.py
@@ -2,28 +2,25 @@ from neopyxel import NeopyxelRelay
 import time
 from neopyxel.effects import SteadyColor, Fireplace, Raining
 from neopyxel.transitions import expand
+import sys
 
 relay = NeopyxelRelay()
 relay.add_stripe(30, 4)
 relay.add_stripe(30, 5)
 relay.add_stripe(30, 6)
 
-# relay.set_pixel_color(list(range(0, 30)), (0, 0, 0))
-# relay.show()
-# # time.sleep(1)
-# relay.set_pixel_color(list(range(0, 30)), (240, 80, 30))
-# relay.show()
 ambar = (240, 80, 30)
 purple = (255, 0, 255)
 fireplace_base = (252, 72, 12)
-# print([str(pixel) for pixel in relay.stripes[1].pixels])
-relay.execute_effect(Fireplace)
-# print([str(pixel) for pixel in relay.stripes[1].pixels])
 
-# relay.execute_effect(SteadyAmbar)
+try:
+    relay.execute_effect(Fireplace)
+    # relay.execute_effect(SteadyAmbar)
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    relay.stop_effect()
+    relay.flush_stripes()
+    print("Ctrl+C pressed...exit")
+    sys.exit(1)
 
-# relay.flush_stripes()
-
-
-# TODO
-# abstract pixel with stripe % segments

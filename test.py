ce74ccbcebba43eb7bba6e04d38b6927d6350a45
vinicius Domingos Alves <vinicius.alves@navarratech.com>
2020-11-02 14:05:20 -0300

    mist

--- test.py
+++ test.py
@@ -4,18 +4,19 @@ from neopyxel.effects import SteadyColor, Fireplace, Raining
 from neopyxel.transitions import expand
 import sys
 
-relay = NeopyxelRelay()
+relay = NeopyxelRelay(debug=True)
 relay.add_stripe(30, 4)
 relay.add_stripe(30, 5)
 relay.add_stripe(30, 6)
 
 ambar = (240, 80, 30)
+white = (255, 255, 255)
 purple = (255, 0, 255)
 fireplace_base = (252, 72, 12)
-
+cyberpunk = (19, 62, 124)
 try:
     # relay.execute_effect(Fireplace)
-    relay.execute_effect(SteadyColor, 
+    relay.execute_effect(SteadyColor,
                          color=ambar,
                          StartTransition=expand.FromCenter,
                          StopTransition=expand.FromBorders)
@@ -26,4 +27,3 @@ except KeyboardInterrupt:
     relay.flush_stripes()
     print("Ctrl+C pressed...exit")
     sys.exit(1)
-

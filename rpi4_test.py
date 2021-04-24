b122718bdf774413e4dc44f3e550d3eaab502765
Vinicius Alves <vinicius.alves.contato@gmail.com>
2021-04-24 18:37:27 -0300

    Add manual test for rpi4 backend

---
+++ rpi4_test.py
@@ -0,0 +1,32 @@
+from neopyxel import NeopyxelRelay
+import time
+from neopyxel.effects import SteadyColor, Fireplace, Raining
+from neopyxel.transitions import expand
+from neopyxel.backend import ArduinoRelayBackend, RaspberryPiBackend
+import sys
+
+relay = NeopyxelRelay(Backend=RaspberryPiBackend, debug=True)
+relay.add_stripe(30, 13, PWM_CHANNEL=1, DMA_CHANNEL=5)
+relay.add_stripe(30, 18, PWM_CHANNEL=0, DMA_CHANNEL=6)
+relay.add_stripe(30, 19, PWM_CHANNEL=1, DMA_CHANNEL=10)
+
+ambar = (240, 80, 30)
+whiter_ambar = (240, 120, 40)
+white = (255, 255, 255)
+purple = (255, 0, 255)
+fireplace_base = (252, 72, 12)
+cyberpunk = (19, 62, 124)
+try:
+    # relay.execute_effect(Fireplace)
+    relay.execute_effect(SteadyColor,
+                         color=ambar,
+                         StartTransition=expand.FromCenter,
+                         StopTransition=expand.FromBorders
+                        )
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    relay.stop_effect()
+    relay.flush_stripes()
+    print("Ctrl+C pressed...exit")
+    sys.exit(1)
